3e26e1265de165b152ef5f18001b76ef2cc2951d
rfaoktvian <rafaoktavian2005@gmail.com>
2026-08-22 23:18:26 +0700

    update: dbt silver layer

--- setup/setup_bigquery.py
+++ setup/setup_bigquery.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PROJECT_ID = os.getenv("GCP_PROJECT_ID")
-LOCATION = os.getenv("GCP_LOCATION", "asia-southeast1") #region sesuaikan bucket
+LOCATION = os.getenv("GCP_LOCATION", "asia-southeast1")
 
 DATASETS = {
     "bronze": "Raw data, apa adanya dari hasil extract (bronze layer)",
